95a30d8b615c2f139bcfa5c32d72884948e51d06
Karelgit <karel0522@gmail.com>
2016-09-09 11:03:41 +0800

    TaskEntity添加到webmagic

--- ZKCrawler-Webmagic/src/main/java/com/gy/wm/Application.java
+++ ZKCrawler-Webmagic/src/main/java/com/gy/wm/Application.java
@@ -13,7 +13,6 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext ctx =
-                SpringApplication.run(Application.class,args);
+        ApplicationContext ctx = SpringApplication.run(Application.class,args);
     }
 }
