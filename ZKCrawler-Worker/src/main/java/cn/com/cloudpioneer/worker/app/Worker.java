e60415a9a3a6030ad81cd07da659482235976c0c
gonali <gonali@gonali.com>
2016-09-19 14:53:13 +0800

    修改任务配置从文件存储配置信息转为数据库存储配置信息。

--- ZKCrawler-Worker/src/main/java/cn/com/cloudpioneer/worker/app/Worker.java
+++ ZKCrawler-Worker/src/main/java/cn/com/cloudpioneer/worker/app/Worker.java
@@ -441,7 +441,7 @@ public class Worker implements Closeable, ConnectionStateListener {
 
                 try {
                     LOGGER.info("=====>> Start request Webmagic.......");
-                    HttpClientUtils.jsonPostRequest(API_CRAWLER_TASK_STARTER, task.getEntityString());
+                    HttpClientUtils.jsonPostRequest(API_CRAWLER_TASK_STARTER, task.toString());
                     LOGGER.info("=====>> Ended request Webmagic.......");
                 } catch (Exception e) {
                     e.printStackTrace();
