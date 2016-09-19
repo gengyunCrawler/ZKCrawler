784de599e1ecb638a08b46548b2d2fb8380103ab
wangtijun <wangtijun@cloudpineer.com.cn>
2016-09-19 14:35:04 +0800

    集成PhantomJS

--- ZKCrawler-Webmagic/src/main/java/com/gy/wm/downloader/SeleniumDownloader.java
+++ ZKCrawler-Webmagic/src/main/java/com/gy/wm/downloader/SeleniumDownloader.java
@@ -3,6 +3,8 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Task;
@@ -13,38 +15,57 @@ import us.codecraft.webmagic.utils.UrlUtils;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 /**
 
- * 需要下载Selenium driver支持。<br>
+ * 需要下载pantomjs driver支持。<br>
  *
  * @author code4crafter@gmail.com <br>
  *         Date: 13-7-26 <br>
  *         Time: 下午1:37 <br>
  */
 public class SeleniumDownloader implements Downloader, Closeable {
+private final static org.slf4j.Logger LOG= LoggerFactory.getLogger(SeleniumDownloader.class);
+    static {
+        InputStream is= System.class.getResourceAsStream("/webdriver.properties");
+        Properties properties=new Properties();
+        try
+        {
+            properties.load(is);
+            System.setProperty("phantomjs.binary.path",properties.getProperty("phantomjs.binary.path"));
+        } catch (IOException e)
+        {  LOG.error("must have pantomjs webdriver path be indicated in /webdriver.properties");
+            e.printStackTrace();
+        }
+    }
     private Logger logger = Logger.getLogger(getClass());
 
     private int sleepTime = 0;
 
     private int poolSize = 1;
 
-    private static final String DRIVER_PHANTOMJS = "phantomjs";
     private WebDriver webDriver;
 
-    public void setWebDriver(WebDriver webDriver)
-    {
-        this.webDriver = webDriver;
-    }
+    //根据每个domain来管理 WebDriver的manmage
+    private Map<String,WebDriver> webDriverMap= Collections.synchronizedMap(new HashMap<String, WebDriver>());
 
-    /**
-     * 新建
-     *
-     * @param chromeDriverPath chromeDriverPath
-     */
-    public SeleniumDownloader(String chromeDriverPath) {
-        System.getProperties().setProperty("webdriver.chrome.driver",
-                chromeDriverPath);
+
+    private WebDriver getWebDriver(Task task){
+        String domain=task.getSite().getDomain();
+        WebDriver driver=  webDriverMap.get(domain);
+        if (driver==null)
+            synchronized (this) {
+                webDriver = create();
+                webDriverMap.put(domain, create());
+                driver = webDriver;
+            }
+        return driver;
     }
 
     /**
@@ -53,8 +74,6 @@ public class SeleniumDownloader implements Downloader, Closeable {
      * @author bob.li.0718@gmail.com
      */
     public SeleniumDownloader() {
-//        System.setProperty("phantomjs.binary.path",
-//         "/Users/Bingo/Downloads/phantomjs-1.9.7-macosx/bin/phantomjs");
     }
 
     /**
@@ -70,15 +89,9 @@ public class SeleniumDownloader implements Downloader, Closeable {
 
     @Override
     public Page download(Request request, Task task) {
+        webDriver= this.getWebDriver(task);
         logger.info("downloading page " + request.getUrl());
         webDriver.get(request.getUrl());
-        try
-        {
-            Thread.sleep(3000);
-        } catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
         WebElement webElement = webDriver.findElement(By.xpath("/html"));
         String content = webElement.getAttribute("outerHTML");
         Page page = new Page();
@@ -92,11 +105,30 @@ public class SeleniumDownloader implements Downloader, Closeable {
 
     @Override
     public void setThread(int thread) {
-      //  this.poolSize = thread;
+        this.poolSize = thread;
     }
 
     @Override
     public void close() throws IOException {
         //webDriverPool.closeAll();
+    }
+    //create webDriver
+    private WebDriver create(){
+        WebDriver webDriver=new PhantomJSDriver();
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        return webDriver;
+    }
+    //when finished all tasks，close all webdrivers and kill all webdriver processes
+
+    public void  closeAll(){
+        for (String domain:webDriverMap.keySet()){
+            WebDriver driver=webDriverMap.remove(domain);
+            if(driver!=null){
+                driver.quit();
+                driver.close();
+                driver=null;
+            }
+
+        }
     }
 }
