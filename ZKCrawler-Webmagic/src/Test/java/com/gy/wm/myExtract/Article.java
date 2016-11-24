2bfeef737492d06c15169911da15abbec8b16c54
tijunwang <15761635397@163.com>
2016-11-24 14:46:32 +0800

    添加测试

---
+++ ZKCrawler-Webmagic/src/Test/java/com/gy/wm/myExtract/Article.java
@@ -0,0 +1,53 @@
+package com.gy.wm.myExtract;
+
+/**
+ * Created by Tijun on 2016/11/18.
+ * @author TijunWang
+ */
+public class Article {
+    private String title;
+    private String author;
+    private String publishTime;
+    private String source;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(String publishTime) {
+        this.publishTime = publishTime;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
